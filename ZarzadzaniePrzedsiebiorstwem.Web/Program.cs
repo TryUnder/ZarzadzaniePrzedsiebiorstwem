425bfd4d8b3bae6f0a49c0c1722949ab16d997f0
DeTry <sniacyinaczej@gmail.com>
2023-06-02 12:32:18 +0200

    Edycja startowego pliku Program.cs

--- ZarzadzaniePrzedsiebiorstwem.Web/Program.cs
+++ ZarzadzaniePrzedsiebiorstwem.Web/Program.cs
@@ -1,25 +1,34 @@
-
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-
+var connectionString = builder.Configuration.GetConnectionString("MyConnection");
+builder.Services.AddDbContext<MyDbContext>(x => x.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+/*
+ Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment()) {
 	app.UseExceptionHandler("/Home/Error");
 }
+*/
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
+/*
 app.MapControllerRoute(
 	name: "default",
 	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+*/
+
+app.MapDefaultControllerRoute();
 
 app.Run();

7e96be1d7883aa2d8adb750942b98868ab319bbc
DeTry <sniacyinaczej@gmail.com>
2023-06-02 11:18:58 +0200

    Praca dyplomowa - utworzenie startowego projektu do zarzadzania przedsiębiorstwem.

---
+++ ZarzadzaniePrzedsiebiorstwem.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace ZarzadzaniePrzedsiebiorstwem.Web.Models {
+	public class ErrorViewModel {
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}

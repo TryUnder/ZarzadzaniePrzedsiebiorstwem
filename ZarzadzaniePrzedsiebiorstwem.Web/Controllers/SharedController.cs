29bec5c505cb036040fa8bfee55b83411604f97e
DeTry <sniacyinaczej@gmail.com>
2023-07-06 19:26:03 +0200

    Próba rozwiązania problemu dotyczącego odświeżania layoutu

---
+++ ZarzadzaniePrzedsiebiorstwem.Web/Controllers/SharedController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZarzadzaniePrzedsiebiorstwem.Web.Controllers {
+	public class SharedController : Controller {
+		public IActionResult AuthenticationStatus() {
+			return PartialView("Partials/_AuthenticationStatus");
+		}
+	}
+}

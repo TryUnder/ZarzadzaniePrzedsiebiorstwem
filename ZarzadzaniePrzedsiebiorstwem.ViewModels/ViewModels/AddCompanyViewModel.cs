ac452eae9eb56fc09cd7cc7908019eecd1198ad8
DeTry <sniacyinaczej@gmail.com>
2023-08-24 13:45:19 +0200

    1) Aktualizacja widoku AddViewControler 2) Aktualizacja CompanyController

---
+++ ZarzadzaniePrzedsiebiorstwem.ViewModels/ViewModels/AddCompanyViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZarzadzaniePrzedsiebiorstwem.Model.Authentication;
+using ZarzadzaniePrzedsiebiorstwem.Model.DataModels;
+
+namespace ZarzadzaniePrzedsiebiorstwem.ViewModels.ViewModels
+{
+    public class AddCompanyViewModel
+    {
+        public User User { get; set; }
+        public Przedsiebiorstwo Przedsiebiorstwo { get; set; }
+    }
+}
--- ZarzadzaniePrzedsiebiorstwem.Web/Controllers/CompanyController.cs
+++ ZarzadzaniePrzedsiebiorstwem.Web/Controllers/CompanyController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using ZarzadzaniePrzedsiebiorstwem.Services.Interfaces;
+using ZarzadzaniePrzedsiebiorstwem.Model.Authentication;
+using ZarzadzaniePrzedsiebiorstwem.Model.DataModels;
+using ZarzadzaniePrzedsiebiorstwem.ViewModels.ViewModels;
 
 namespace ZarzadzaniePrzedsiebiorstwem.Web.Controllers {
     public class CompanyController : Controller {
@@ -11,8 +14,15 @@ namespace ZarzadzaniePrzedsiebiorstwem.Web.Controllers {
             this.userService = userService;
         }
 
-        public IActionResult AddCompanyView() { 
-            return View("AddCompanyView");
+        public IActionResult AddCompanyView(User user) {
+
+            var emptyCompany = new Przedsiebiorstwo();
+
+            var addCompanyViewModel = new AddCompanyViewModel {
+                User = user,
+                Przedsiebiorstwo = emptyCompany
+            };
+            return View("AddCompanyView", addCompanyViewModel);
         }
     }
 }

49e245a0ccee4dd88985b19029caa524004bf61c
DeTry <sniacyinaczej@gmail.com>
2023-09-14 13:01:11 +0200

    update

---
+++ ZarzadzaniePrzedsiebiorstwem.Model/DataModels/Planer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZarzadzaniePrzedsiebiorstwem.Model.DataModels {
+    public class Planer {
+        public int Id { get; set; }
+
+        public virtual ICollection<String> Zadanie { get; set; }
+        public virtual ICollection<String>? Podzadanie { get; set; }
+        public virtual ICollection<String>? Tagi { get; set; }
+        public DateTime? Termin { get; set; }
+    }
+}

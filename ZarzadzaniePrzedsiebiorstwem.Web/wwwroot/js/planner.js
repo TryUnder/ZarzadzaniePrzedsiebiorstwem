471a71638452319e3b5373b7b91d2feb04de2e08
DeTry <sniacyinaczej@gmail.com>
2023-09-17 20:26:47 +0200

    update

---
+++ ZarzadzaniePrzedsiebiorstwem.Web/wwwroot/js/planner.js
@@ -0,0 +1,84 @@
+﻿
+function HideElement(id) {
+    const element = document.getElementById(id);
+    element.style.display = "none";
+}
+
+function HidePlaceholder(id) {
+    const element = document.getElementById(id);
+    element.dataset.placeholder = element.placeholder;
+    element.placeholder = "";
+}
+
+function ShowElement(id, idInput) {
+    const element = document.getElementById(id);
+    const elementInput = document.getElementById(idInput);
+    if (elementInput.value === '') {
+        element.style.display = "inline-block";
+    }
+}
+
+function ShowPlaceholder(id) {
+    const element = document.getElementById(id);
+    element.placeholder = element.dataset.placeholder;
+}
+
+function ShowIconGlow() {
+    const iconElements = document.querySelectorAll(".task-item-icon");
+    const numberElements = document.querySelectorAll(".task-item-number");
+    const parElements = document.querySelectorAll(".text-item");
+
+    iconElements.forEach((icon, index) => {
+        icon.addEventListener('click', function() {
+
+            iconElements.forEach((el) => {
+                el.classList.remove('clicked');
+            });
+
+            numberElements.forEach((el) => {
+                el.classList.remove('clicked_icon');
+            });
+
+            const numberElement = numberElements[index];
+            icon.classList.add('clicked');
+            numberElement.classList.add('clicked_icon');
+        });
+    });
+
+    numberElements.forEach((number, index) => {
+        number.addEventListener('click', function () {
+
+            iconElements.forEach((el) => {
+                el.classList.remove('clicked');
+            });
+
+            numberElements.forEach((el) => {
+                el.classList.remove('clicked_icon');
+            });
+
+            const iconElement = iconElements[index];
+            iconElement.classList.add('clicked');
+            number.classList.add('clicked_icon');
+        });
+    });
+
+    parElements.forEach((par, index) => {
+        par.addEventListener('click', function () {
+
+            iconElements.forEach((el) => {
+                el.classList.remove('clicked');
+            });
+
+            numberElements.forEach((el) => {
+                el.classList.remove('clicked_icon');
+            });
+
+            const iconElement = iconElements[index];
+            const numberElement = numberElements[index];
+            iconElement.classList.add('clicked');
+            numberElement.classList.add('clicked_icon');
+        });
+    });
+}
+
+window.addEventListener('load', ShowIconGlow);

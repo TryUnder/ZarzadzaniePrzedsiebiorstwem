5e16029be8d569a682a46ca726d74f98ff8c3e0e
DeTry <sniacyinaczej@gmail.com>
2023-09-18 23:07:45 +0200

    update

--- ZarzadzaniePrzedsiebiorstwem.Web/wwwroot/js/planner.js
+++ ZarzadzaniePrzedsiebiorstwem.Web/wwwroot/js/planner.js
@@ -82,3 +82,71 @@ function ShowIconGlow() {
 }
 
 window.addEventListener('load', ShowIconGlow);
+
+function CreateList() {
+    var taskListItems = document.getElementById("task-list-items");
+    var listName = prompt("Podaj nazwę listy");
+    if (listName === '') {
+        return;
+    }
+
+    var newRow = document.createElement("div");
+    newRow.className = "task-list-items-inside";
+
+    var newCol = document.createElement("div");
+    newCol.className = "task-list-items-inside-item-first list-item-bcg";
+
+    var colImageNode = document.createElement("div");
+    colImageNode.className = "color-item-config";
+
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+
+    colImageNode.style.backgroundColor = `rgb(${ r }, ${ g }, ${ b })`;
+
+    var newPar = document.createElement("p");
+    newPar.className = "task-list-items-inside-item-second text-list-item";
+    newPar.textContent = listName;
+
+    var thirdPar = document.createElement("p");
+    thirdPar.className = "task-list-items-inside-item-third list-item-number";
+    thirdPar.textContent = "12";
+
+    newCol.appendChild(colImageNode);
+    newRow.appendChild(newCol);
+    newRow.appendChild(newPar);
+    newRow.appendChild(thirdPar);
+
+    taskListItems.insertBefore(newRow, document.getElementById("before"));
+}
+
+function CreateTag() {
+    var tagsContainerFlex = document.getElementById("menu-tags-container-flex");
+
+    var rows = tagsContainerFlex.getElementsByClassName("menu-tags-row");
+
+    if (rows === 0 || rows[rows.length - 1].querySelectorAll("div.simple_tag_1").length >= 3) {
+        var menuTagsContainer = document.getElementById("menu-tags-container");
+        var newRow = document.createElement("div");
+        newRow.className = "menu-tags-row menu-tags-container-flex"
+    }
+
+    var tagName = prompt("Podaj nazwę tagu");
+
+
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+
+    var newTag = document.createElement("div");
+    newTag.className = "simple_tag_1";
+    newTag.style.backgroundColor = `rgb(${ r }, ${ g }, ${ b })`;
+
+    var newPar = document.createElement("p");
+    newPar.textContent = tagName;
+
+    newTag.appendChild(newPar);
+    newRow.appendChild(newTag);
+    menuTagsContainer.appendChild(newRow);
+}
